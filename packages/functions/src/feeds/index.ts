ad9ad36fbbf4d01401a23750f9d1915555c9abf4
Henry Dowling <htdowling@gmail.com>
2024-12-12 20:21:51 -0800

    add openai

--- packages/functions/src/feeds/index.ts
+++ packages/functions/src/feeds/index.ts
@@ -2,7 +2,9 @@ import { Anthropic } from './anthropic';
 import { DatadogUS1 } from './datadog-us1';
 import { GCP } from './gcp';
 import { GitHub } from './github';
+import { OpenAI } from './openai';
 import { SendGrid } from './sendgrid';
 import { Snowflake } from './snowflake';
 
-export default [new GitHub(), new GCP(), new SendGrid(), new DatadogUS1(), new Snowflake(), new Anthropic()];
+
+export default [new GitHub(), new GCP(), new SendGrid(), new DatadogUS1(), new Snowflake(), new Anthropic(), new OpenAI()];
